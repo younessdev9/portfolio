ff3701df97523e29061331468f3a0643adfc3200
youness.hassani <youness.hassani@4d.com>
2021-10-26 14:54:00 +0100

    adding Google analytics integration

--- pages/_app.js
+++ pages/_app.js
@@ -16,19 +16,22 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRUCKING_ID}`}
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
-      <Script strategy="lazyOnload">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRUCKING_ID}', {
-              page_path: window.location.pathname,
-            });
-        `}
-      </Script>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+        });
+    `,
+        }}
+      />
       <ThemeProvider theme={themeMode}>
         <GlobalStyles currentTheme={theme} />
         <NavBar
